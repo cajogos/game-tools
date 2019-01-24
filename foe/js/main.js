548c1518d6f23aedc7c585bc2eb1afd07cb8acf4
Carlos Ferreira <c@rlos.rocks>
2019-01-24 23:22:34 +0000

    Some tweaks to the outputted results

--- foe/js/main.js
+++ foe/js/main.js
@@ -6,19 +6,55 @@ for (var i = 0; i < neededResources.length; i++)
 	if (typeof totalsNeeded[name] === 'undefined')
 	{
 		totalsNeeded[name] = {
-			amount: 0,
+			needed: 0,
 			owned: getOwnedResourceAmount(name)
 		};
 	}
-	totalsNeeded[name].amount += amount;
+	totalsNeeded[name].needed += amount;
 }
 
 for (var name in totalsNeeded)
 {
 	if (totalsNeeded.hasOwnProperty(name))
 	{
-		totalsNeeded[name].required = totalsNeeded[name].amount - totalsNeeded[name].owned;
+		totalsNeeded[name].required = totalsNeeded[name].needed - totalsNeeded[name].owned;
 	}
 }
 
-console.table(totalsNeeded);
+// Calculate available resources to trade
+var requiredResources = [];
+var available = [];
+for (var i = 0; i < ownedResources.length; i++)
+{
+	var name = ownedResources[i].name;
+	var amount = ownedResources[i].amount;
+	var needed = 0;
+
+	if (typeof totalsNeeded[name] !== 'undefined')
+	{
+		amount = totalsNeeded[name].owned - totalsNeeded[name].needed;
+		needed = totalsNeeded[name].needed;
+		if (totalsNeeded[name].required > 0)
+		{
+			requiredResources[name] = totalsNeeded[name].required;
+		}
+	}
+
+	if (amount <= 0)
+	{
+		continue;
+	}
+
+	available[name] = {
+		owned: ownedResources[i].amount,
+		needed: needed,
+		available: amount
+	};
+}
+
+console.warn('*** REQUIRED ***');
+console.table(requiredResources);
+
+console.warn('*** AVAILABLE ***');
+console.table(available);
+
--- foe/js/owned-resources.js
+++ foe/js/owned-resources.js
@@ -10,7 +10,7 @@ ownedResources.push({ name: "wine", amount: 18 });
 // Iron Age
 ownedResources.push({ name: "cloth", amount: 40 });
 ownedResources.push({ name: "ebony", amount: 12 });
-ownedResources.push({ name: "jewelry", amount: 26 });
+ownedResources.push({ name: "jewelry", amount: 10 });
 ownedResources.push({ name: "iron", amount: 40 });
 ownedResources.push({ name: "limestone", amount: 39 });
 
@@ -19,10 +19,10 @@ ownedResources.push({ name: "copper", amount: 1 });
 ownedResources.push({ name: "gold", amount: 19 });
 ownedResources.push({ name: "granite", amount: 12 });
 ownedResources.push({ name: "honey", amount: 51 });
-ownedResources.push({ name: "alabaster", amount: 110 });
+ownedResources.push({ name: "alabaster", amount: 88 });
 
 // High Middle Ages
-ownedResources.push({ name: "bricks", amount: 57 });
+ownedResources.push({ name: "bricks", amount: 37 });
 ownedResources.push({ name: "glass", amount: 88 });
 ownedResources.push({ name: "dried herbs", amount: 15 });
 ownedResources.push({ name: "ropes", amount: 36 });
